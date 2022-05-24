165dc52552eff8c96c5726616821bea5460a4f2c
zyr3210115527 <101702244+zyr3210115527@users.noreply.github.com>
2022-05-24 21:24:17 +0800

    changed jibs

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,15 +1,16 @@
 n=1
-while (n**2+n+2)/2<114514:
+#Set an initial value.
+while (n**2+n+2)/2<114514: #Start the loop
     if (n**2+n+2)/2>=64:
         message_a="We've cut it "+str(n)+" times"+" and we have "+str((n**2+n+2)/2)+" pizza now, it's enough for us."
-        print(message_a)
+        print(message_a)#When the pizza quantity reaches the requirement, this result is output
         break
     elif n==1: #Solve the problem of singular and plural word "word"
         message_b="We've cut it "+str(n)+" time"+" and we have "+str((n**2+n+2)/2)+" pizza now, it isn't enough for us."
         print(message_b)
-        n+=1
+        n+=1 #The pizza quantity does not meet the requirements, the output results leave this cycle and enter the next cycle
     else:
         message_c="We've cut it "+str(n)+" times"+" and we have "+str((n**2+n+2)/2)+" pizza now, it isn't enough for us."
         print(message_c)
-        n+=1
+        n+=1 #The pizza quantity does not meet the requirements, the output results leave this cycle and enter the next cycle
  
--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,4 +1,6 @@
 for b in range(1,11,1):
-    a=((1+b)*b)/2 #Use the method of Gauss algorithm
+#In order to get the number of ten triangles, the cycle should be carried out ten times
+    a=((1+b)*b)/2
+#Use the method of Gauss algorithm
     print(a)
-
+#get each result
