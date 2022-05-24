0151211265f39438515277af0de22cd4329f8bd3
zyr3210115527 <101702244+zyr3210115527@users.noreply.github.com>
2022-05-24 21:32:06 +0800

    Delete pratical8 directory

--- pratical8/fragment_counter.py
+++
@@ -1,7 +0,0 @@
-import re
-
-gene='ATGCAATCGACTACGATCAATCGAGGGCC'
-a = re.findall('GAATTC',gene)
-#Find qualified sequences
-print("The number of the fragment(s) is "+str(len(a)+1))
-#Output the number of the fragment(s).
